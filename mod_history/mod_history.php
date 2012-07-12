c740b6a0a1aefe79116fc163e96e26edd539b241
Chris French <chris@ammonitenetworks.com>
2012-07-12 16:54:47 -0700

    cleaning up  before tagging

--- mod_history/mod_history.php
+++ mod_history/mod_history.php
@@ -1,24 +1,23 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_custom
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @version	1.5
+ * @package	History Module
+ * @author 	Dioscouri Design
+ * @link 	http://www.dioscouri.com
+ * @copyright Copyright (C) 2007 Dioscouri Design. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  */
 
 // no direct access
-defined('_JEXEC') or die;
+defined('_JEXEC') or die ;
 
-require_once dirname(__FILE__).'/helper.php';
+require_once dirname(__FILE__) . '/helper.php';
 // Get the breadcrumbs
-$list	= modHistoryHelper::getList($params);
-$count	= count($list);
+$list = modHistoryHelper::getList($params);
+$count = count($list);
 // Set the default separator
-$separator = modHistoryHelper::setSeparator($params->get('separator'));	
+$separator = modHistoryHelper::setSeparator($params -> get('separator'));
 
+$moduleclass_sfx = htmlspecialchars($params -> get('moduleclass_sfx'));
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
-
-
-
-require JModuleHelper::getLayoutPath('mod_history', $params->get('layout', 'default'));
+require JModuleHelper::getLayoutPath('mod_history', $params -> get('layout', 'default'));
